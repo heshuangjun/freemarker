97f563dbd4881fb1397c4ae12706d7b12533b420
heshuangjun <844440417@qq.com>
2018-11-09 14:32:35 +0800

    freemarker_第一次提交

---
+++ src/main/java/cn/itcast/demo/FreemarkerTest.java
@@ -0,0 +1,49 @@
+package cn.itcast.demo;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author heshuangjun
+ * @date 2018-11-09 14:00
+ */
+public class FreemarkerTest {
+
+    public static void main(String[] args) throws IOException, TemplateException {
+
+        //第一步:创建一个Configuration对象.构造方法的参数就是freemarker的版本号
+        Configuration configuration = new Configuration(Configuration.getVersion());
+
+        //第二步:设置模板文件的路径
+        configuration.setDirectoryForTemplateLoading(new File("/Users/heshuangjun/IDEA_workspace01/freeMarker_Demo/src/main/resources"));
+
+        //第三步:设置字符集编码utf-8
+        configuration.setDefaultEncoding("utf-8");
+
+        //第四步:加载一个模板,创建一个模板对象
+        Template template = configuration.getTemplate("test.ftl");
+
+        //第五步:组装数据,用Map组装
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("name","苏珊");
+
+        //第六步:组装一个输出流,达到结果是为了生成一个html页面
+        Writer out= new FileWriter("/Users/heshuangjun/IDEA_workspace01/freeMarker_Demo/src/main/resources/test.html");
+
+        //第七步:调用模板对象,输出文件
+        template.process(map,out);
+
+        //第八步:关闭流
+        out.close();
+
+
+    }
+}
---
+++ src/main/resources/test.ftl
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>freemarker模板</title>
+</head>
+<body>
+    <#-- freemarker插值的格式: ${...} -->
+    ${name}
+    Hello!What a mysterious world it is!
+    There had the lovable micro-organism , u will like them!
+    The cute little opossum has a number of tricks up its defensive sleeve
+    I like many animals.
+    They are tiger, monkey, dog, lion, elephant and so on… Dog is my favorite animal.
+    Because it is very lovely and honest.
+    The cute little opossum has a number of tricks up its defensive sleeve.
+    Thank you!
+</body>
+</html>
